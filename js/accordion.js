a15e7647fc5791e314b5d68c1290b8adf8b1a22b
LAPTOP-3EN547C2\Natalie <natalie930302@gmail.com>
2023-05-31 22:38:42 +0800

    no message

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,8 @@
 const accordionItems = document.querySelectorAll(".accordion .item");
 accordionItems.forEach((item, index) => {
-  item.addEventListener("click", () => clickAccordionItem(item, index));
+  item
+    .querySelector(".icon")
+    .addEventListener("click", () => clickAccordionItem(item, index));
 });
 
 function clickAccordionItem(item, index) {
