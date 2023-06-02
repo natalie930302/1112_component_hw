6e3d8bb89a767cabc8b8f25fbc73a4f2b269669d
LAPTOP-3EN547C2\Natalie <natalie930302@gmail.com>
2023-06-02 13:56:51 +0800

    no message

---
+++ js/carousel.js
@@ -0,0 +1,25 @@
+const carousel = body.querySelector(".carousel");
+const carouselRightBtn = carousel.querySelector(".btn--right");
+const carouselLeftBtn = carousel.querySelector(".btn--left");
+const carouselItems = carousel.querySelectorAll(".carousel_row .item");
+const carouselDots = carousel.querySelectorAll(".dots .dot");
+let activeCarouselItemIndex = Array.from(carouselItems).indexOf(
+  carousel.querySelector(".carousel_row .item.active")
+);
+
+function changeCarouselItem(direction) {
+  carouselItems[activeCarouselItemIndex].classList.remove("active");
+  carouselDots[activeCarouselItemIndex].classList.remove("active");
+  activeCarouselItemIndex = activeCarouselItemIndex + direction;
+  if (activeCarouselItemIndex >= carouselItems.length) {
+    activeCarouselItemIndex = 0;
+  }
+  if (activeCarouselItemIndex < 0) {
+    activeCarouselItemIndex = carouselItems.length - 1;
+  }
+  carouselItems[activeCarouselItemIndex].classList.add("active");
+  carouselDots[activeCarouselItemIndex].classList.add("active");
+}
+
+carouselRightBtn.addEventListener("click", () => changeCarouselItem(1));
+carouselLeftBtn.addEventListener("click", () => changeCarouselItem(-1));
