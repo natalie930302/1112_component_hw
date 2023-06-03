cd3c6eaf9916045a0536d3d36c187df64ef2289c
LAPTOP-3EN547C2\Natalie <natalie930302@gmail.com>
2023-06-03 15:47:40 +0800

    no message

--- js/pagination.js
+++ js/pagination.js
@@ -3,14 +3,15 @@ const paginationRightBtn = pagination.querySelector(".btn-nav.next");
 const paginationLeftBtn = pagination.querySelector(".btn-nav.prev");
 const paginationRow = pagination.querySelector(".page-numbers");
 let paginationItems = paginationRow.querySelectorAll(".btn-page");
-let activePaginationPage = 4;
-let maxPaginationPage = 30;
+let activePaginationPage = 2;
+let maxPaginationPage = 23;
 
 function setPagination(pageNum) {
   activePaginationPage = pageNum;
   var paginationHTML = ``;
-  if (activePaginationPage <= 4) {
-    for (var i = 1; i <= 5; i++) {
+
+  if (maxPaginationPage <= 7) {
+    for (var i = 1; i <= maxPaginationPage; i++) {
       var partHTML =
         '<button class="btn-page ' +
         (i == activePaginationPage ? "active" : "") +
@@ -19,47 +20,63 @@ function setPagination(pageNum) {
         "</button>";
       paginationHTML = paginationHTML + partHTML;
     }
-    paginationHTML =
-      paginationHTML +
-      '<span class="dots">...</span><button class="btn-page">' +
-      maxPaginationPage +
-      "</button>";
-  }
-  if (activePaginationPage > maxPaginationPage - 4) {
-    paginationHTML =
-      paginationHTML +
-      '<button class="btn-page">1</button><span class="dots">...</span>';
-    for (var i = maxPaginationPage - 4; i <= maxPaginationPage; i++) {
-      var partHTML =
-        '<button class="btn-page ' +
-        (i == activePaginationPage ? "active" : "") +
-        '">' +
-        i +
+  } else {
+    if (activePaginationPage <= 4) {
+      for (var i = 1; i <= 5; i++) {
+        var partHTML =
+          '<button class="btn-page ' +
+          (i == activePaginationPage ? "active" : "") +
+          '">' +
+          i +
+          "</button>";
+        paginationHTML = paginationHTML + partHTML;
+      }
+      paginationHTML =
+        paginationHTML +
+        '<span class="dots">...</span><button class="btn-page">' +
+        maxPaginationPage +
         "</button>";
-      paginationHTML = paginationHTML + partHTML;
     }
-  }
-  if (
-    activePaginationPage > 4 &&
-    activePaginationPage <= maxPaginationPage - 4
-  ) {
-    paginationHTML =
-      paginationHTML +
-      '<button class="btn-page">1</button><span class="dots">...</span>';
-    for (var i = activePaginationPage - 1; i <= activePaginationPage + 1; i++) {
-      var partHTML =
-        '<button class="btn-page ' +
-        (i == activePaginationPage ? "active" : "") +
-        '">' +
-        i +
+    if (activePaginationPage > maxPaginationPage - 4) {
+      paginationHTML =
+        paginationHTML +
+        '<button class="btn-page">1</button><span class="dots">...</span>';
+      for (var i = maxPaginationPage - 4; i <= maxPaginationPage; i++) {
+        var partHTML =
+          '<button class="btn-page ' +
+          (i == activePaginationPage ? "active" : "") +
+          '">' +
+          i +
+          "</button>";
+        paginationHTML = paginationHTML + partHTML;
+      }
+    }
+    if (
+      activePaginationPage > 4 &&
+      activePaginationPage <= maxPaginationPage - 4
+    ) {
+      paginationHTML =
+        paginationHTML +
+        '<button class="btn-page">1</button><span class="dots">...</span>';
+      for (
+        var i = activePaginationPage - 1;
+        i <= activePaginationPage + 1;
+        i++
+      ) {
+        var partHTML =
+          '<button class="btn-page ' +
+          (i == activePaginationPage ? "active" : "") +
+          '">' +
+          i +
+          "</button>";
+        paginationHTML = paginationHTML + partHTML;
+      }
+      paginationHTML =
+        paginationHTML +
+        '<span class="dots">...</span><button class="btn-page">' +
+        maxPaginationPage +
         "</button>";
-      paginationHTML = paginationHTML + partHTML;
     }
-    paginationHTML =
-      paginationHTML +
-      '<span class="dots">...</span><button class="btn-page">' +
-      maxPaginationPage +
-      "</button>";
   }
   paginationRow.innerHTML = paginationHTML;
   paginationItems = paginationRow.querySelectorAll(".btn-page");
